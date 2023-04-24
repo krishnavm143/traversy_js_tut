d8f1b1dcc0bea437ef3fb895b6d68abb7183ff0a
krishna <krishnavm143@gmail.com>
2023-04-24 07:07:00 +0530

    no changes

--- javascript-sandbox-start/01-variables-data-types/09-strings/script.js
+++ javascript-sandbox-start/01-variables-data-types/09-strings/script.js
@@ -1,11 +1,11 @@
 let x;
 
 
-const name="krishna"
-const age=20
+const name = "krishna"
+const age = 20
 
 // x='My name is '+ name + ' and my age is '+age
 
-x=`My Name is ${name} and my age is ${age}`
+x = `My Name is ${name} and my age is ${age}`
 
 console.log(x)
